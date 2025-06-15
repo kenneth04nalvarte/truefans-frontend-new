cceb855b323646cc97969fae265fae8f5cd34366
kenne <kenneth.nalvarte.1@gmail.com>
2025-06-15 17:35:58 -0600

    fix: add placeholder Homepage.js to resolve build error

---
+++ truefans/truefans/src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Homepage() {
+  return (
+    <div>
+      <h2>Homepage Component</h2>
+      <p>This is a placeholder for the Homepage component.</p>
+    </div>
+  );
+}
+
+export default Homepage; 
