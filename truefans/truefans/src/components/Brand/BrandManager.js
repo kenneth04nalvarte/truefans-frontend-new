2afbf6eaed5738d6b45201654b196063925d0cdd
kenne <kenneth.nalvarte.1@gmail.com>
2025-06-15 17:43:33 -0600

    fix: add placeholder BrandManager.js to resolve build error

---
+++ truefans/truefans/src/components/Brand/BrandManager.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function BrandManager() {
+  return (
+    <div>
+      <h2>BrandManager Component</h2>
+      <p>This is a placeholder for the BrandManager component.</p>
+    </div>
+  );
+}
+
+export default BrandManager; 
